03a34d1230a3d46c4570c2e736a721ac75e7b155
Srivardhan S <108318036+SrivardhanS@users.noreply.github.com>
2024-09-25 13:20:58 +0530

    Update DatabaseConnection.java

--- src/bank/DatabaseConnection.java
+++ src/bank/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/BankDB?user=root&password=Shreejith@123";
+    private static final String URL = "jdbc:mysql://localhost:3306/BankDB?user=root&password=srivardhan@123";
 
     static {
         try {
